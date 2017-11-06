77b66f52a71d4261de171009af5157ea31242ad0
sheenanick <sheenanick@gmail.com>
2017-11-06 12:39:01 -0800

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 import Home from './routes/Home/Home';
 import Portfolio from './routes/Portfolio/Portfolio';
 import Contact from './routes/Contact/Contact';
@@ -14,6 +15,7 @@ class App extends Component {
         <Route exact path="/" component={Home}/>
         <Route path="/portfolio" component={Portfolio}/>
         <Route path="/contact" component={Contact}/>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import github from '../../img/icons/github.png';
+import linkedin from '../../img/icons/linkedin.png';
+import './Footer.css';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='Footer'>
+        <div className='footer-container'>
+          <p className='copyright'>&copy; 2017 Sheena Do</p>
+          <div className='footer-icon-container'>
+            <a href='https://github.com/sheenanick' target='_blank' rel='noopener noreferrer'>
+              <img className='footer-icon' src={github} alt='github'/>
+            </a>
+            <a href='https://www.linkedin.com/in/sheenado/' target='_blank' rel='noopener noreferrer'>
+              <img className='footer-icon' src={linkedin} alt='linkedin'/>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
