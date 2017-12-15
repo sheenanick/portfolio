cfca929da8045b2a983534ac15eecfad5cb9e8cf
sheenanick <sheenanick@gmail.com>
2017-12-15 13:40:51 -0800

    Add hover animation to chevron

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -18,19 +18,14 @@ export default class Home extends Component {
                 <stop offset='0%' style={{stopColor: '#ff4d4d'}} />
                 <stop offset='100%' style={{stopColor: '#ff4d79'}} />
               </linearGradient>
-
               <linearGradient id='gradient2' x1='0%' y1='0%' x2='0%' y2='100%'>
-                <stop offset='0%' style={{stopColor: '#ff4da6'}}>
-                  <animate xlinkHref='#chev2' attributeName="stopColor" values="#ffffff, #ffffff" dur="4s" repeatCount="indefinite" />
-                  <animate attributeName="offset" values=".95,.80,.60,.40,.20,0,.20,.40,.60,.80,.95" dur="14s" repeatCount="indefinite" />
-                </stop>
+                <stop offset='0%' style={{stopColor: '#ff4da6'}} />
                 <stop offset='100%' style={{stopColor: '#ff4d79'}} />
               </linearGradient>
-
             </defs>
             <g>
             	<polygon className='chevron-top' points='407.437,63.398 386.258,42.15 203.718,224.095 21.179,42.15 0,63.398 203.718,266.453' fill='url(#gradient1)'/>
-              <polygon id='chev2' className='chevron-bottom' points='203.718,322.929 21.179,140.984 0,162.232 203.718,365.287 407.437,162.232 386.258,140.984' fill='url(#gradient2)'/>
+              <polygon className='chevron-bottom' points='203.718,322.929 21.179,140.984 0,162.232 203.718,365.287 407.437,162.232 386.258,140.984' fill='url(#gradient2)'/>
             </g>
           </svg>
         </div>
